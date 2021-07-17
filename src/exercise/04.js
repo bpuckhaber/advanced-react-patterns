3ca91a254b04cd8738404c1781cbbd4e2edc798b
Ben Puckhaber <ben.puckhaber@gmail.com>
2021-07-17 07:19:37 -0500

    exercise.04

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -8,10 +8,8 @@ function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  const togglerProps = {'aria-pressed': on, onClick: toggle}
+  return {on, toggle, togglerProps}
 }
 
 function App() {
@@ -28,8 +26,3 @@ function App() {
 }
 
 export default App
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
